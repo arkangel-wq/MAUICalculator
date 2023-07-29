00be5681999deff77612faba491b8a5cc2939827
Angel's <sada2022as@outlook.com>
2023-07-28 18:40:12 -0600

    Comandos y nugget para realizar las operaciones matematicas

--- MAUICalculator/MVVM/ViewModels/CalcViewModel.cs
+++ MAUICalculator/MVVM/ViewModels/CalcViewModel.cs
@@ -1,4 +1,5 @@
-﻿using PropertyChanged;
+﻿using Dangl.Calculator;
+using PropertyChanged;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,5 +19,27 @@ namespace MAUICalculator.MVVM.ViewModels
         public ICommand OperationCommand =>
             new Command((number) => { Formula += number; });
 
+        public ICommand ResetCommand => new Command(() =>
+        {
+            Result = "0";
+            Formula = "";
+        });
+
+        public ICommand BackSpaceCommand => new Command(() =>
+        {
+            if(Formula.Length > 0)
+            {
+                Formula = Formula.Substring(0, Formula.Length - 1);
+            }
+        });
+
+
+        public ICommand CalculateCommand => new Command(() =>
+        {
+            if (Formula.Length == 0)
+                return;
+            var calculation = Calculator.Calculate(Formula);
+            Result = calculation.Result.ToString();
+        });
     }
 }
